1846a9820fc0d3e4dedd046236b58848238f8924
Akukal <haikal4432.pplg1@smkprestasiprima.sch.id>
2024-10-23 10:29:41 -0700

    modified:   config/database.php

--- config/database.php
+++ config/database.php
@@ -7,12 +7,11 @@ function getDB() {
         'password' => ''
     ];
 
-    try {
-        $db = new PDO("mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8mb4", $config['username'], $config['password']);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        return $db;
-    } catch(PDOException $e) {
-        die("Koneksi database gagal: " . $e->getMessage());
+    $conn = mysqli_connect($config['host'], $config['username'], $config['password'], $config['dbname']);
+
+    if (!$conn) {
+        echo "Koneksi database gagal"();
+    } else {
+        echo "Koneksi database berhasil";
     }
 }
